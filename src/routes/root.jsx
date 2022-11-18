6ec5f8496bb23fac559b84443160630a12926798
narmanguebraun <narmanguebraun1@gmail.com>
2022-11-18 22:13:19 +0100

    create create contact action

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,9 @@
-import { Outlet, Link, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 
+export async function action() {
+  await createContact();
+}
 export async function loader() {
   const contacts = await getContacts();
   return { contacts };
@@ -24,9 +27,9 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
