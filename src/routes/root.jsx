d118432723a88f61c637bffd078cec52df052700
narmanguebraun <narmanguebraun1@gmail.com>
2022-11-18 22:52:09 +0100

    redirect new records to the edit page

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,11 @@
-import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, redirect } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
-  await createContact();
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
 }
+
 export async function loader() {
   const contacts = await getContacts();
   return { contacts };
